b38e49856e873f72af031fdeaccac3f713e859e0
ichabod <mrdoy@mail.ru>
2019-11-09 21:00:52 +0300

    commitClass

---
+++ edu class.js
@@ -0,0 +1,16 @@
+class Crud {
+  constructor(items, rootNode){
+    this.items = items;
+    this.rootNode = rootNode;
+  }
+  this.state.items() {
+    items;
+  };
+  this.state.sort () {
+    field: 'name',
+    direction: 'asc',
+  };
+  this.state.editingId() {
+    return 0;
+  };
+}
--- index.js
+++ index.js
@@ -1,165 +1,186 @@
-let state = {
-  sportsman: [
-    {
-      name:   'Лионель Месси',
-      sport:  'Футбол',
-      salary: '$ 127 млн',
-      id: '1',
-    }, {
-      name:   'Криштиану Роналду',
-      sport:   'Футбол',
-      salary:  '$ 109 млн',
-      id: '2',
-    }, {
-      name:   'Неймар',
-      sport:  'Футбол',
-      salary: '$ 105 млн',
-      id: '3',
-    }, {
-      name:   'Канело Альварес',
-      sport:  'Бокс',
-      salary: '$ 94 млн',
-      id: '4',
-    }, {
-      name:   'Роджер Федерер',
-      sport:  'Большой тенис',
-      salary: '$ 93,4 млн',
-      id: '5',
-    }, {
-      name:   'Рассел Уилсон',
-      sport:  'Американский футбол',
-      salary: '$ 89,5 млн',
-      id: '6',
-    }, {
-      name:   'Аарон Роджерс',
-      sport:  'Американский футбол',
-      salary: '$ 89,3 млн',
-      id: '7',
-    }, {
-      name:   'Леброн Джеймс',
-      sport:  'Баскетбол',
-      salary: '$ 89 млн',
-      id: '8',
-    }, {
-      name:   'Стивен Карри',
-      sport:  'Баскетбол',
-      salary: '$ 79,8 млн',
-      id: '9',
-    }, {
-      name:   'Кевин Дюран',
-      sport:  'Баскетбол',
-      salary: '$ 65,4 млн',
-      id: '10',
+function CRUD(items, rootNode) {
+
+  const state  = {
+    items,
+    sort: {
+      field: 'name',
+      direction: 'asc',
     },
-  ],
-  sort: {
-    field: 'name',
-    direction: 'asc',
-  }, /*Добавление новых спортсменов*/
-  addSportsman: function addSportsman () {
-      lastId++;
-    let newObject = {
-        name:   $('.nameInp').val(),
-        sport:  $('.sportInp').val(),
-        salary: $('.solaryInp').val(),
-        id:  lastId,
-    };
-    setState({...state, sportsman : [ newObject, ...state.sportsman ]});
-  }, /* Сортировака спортсменов*/
-  sortString: function sortString() {
-    const sportsman = [...state.sportsman.sort((a, b) => a.name < b.name ? 1 : -1)];
-    const sort = {...state.sort, direction: 'asc' };
-    setState({...state, sportsman, sort});
+    editingId: 0,
+  };
+
+  let lastId = 10;
+
+  const crud = {
+
+    state,
+    setState: (newState) => {
+      if (crud.state != newState) {
+        crud.state = newState;
+        crud.render();
+      };
+    },
+
+    /*Добавление спорсменов*/
+
+    addSportsman: function addSportsman () {
+        lastId++;
+      let newObject = {
+          name:   $('.nameInp').val(),
+          sport:  $('.sportInp').val(),
+          salary: $('.solaryInp').val(),
+          id:  lastId,
+      };
+      crud.setState({...crud.state, items : [ newObject, ...crud.state.items ]});
+    },
+
+    /* Сортировака спортсменов*/
+
+    sortString: function sortString() {
+      const items = [...crud.state.items.sort((a, b) => a.name < b.name ? 1 : -1)];
+      const sort = {...crud.sort, direction: 'asc' };
+      crud.setState({...crud.state, items, sort});
+    },
+    sortStringRevers: function sortStringRevers() {
+      const items = [...crud.state.items.sort((a, b) => a.name > b.name ? 1 : -1)];
+      const sort = {...crud.sort, direction: 'desc' };
+      crud.setState({...crud.state, items, sort});
+    },
+    choiseMehtodSort: function choiseMehtodSort() {
+        if (crud.state.sort.direction === 'desc') {
+          crud.sortString();
+        } else {
+          crud.sortStringRevers();
+        }
+    },
+
+    /* Удаление строк*/
+
+    deleteSportsman: function deleteSportsman(sportsmanId) {
+      const items = crud.state.items.filter(function(obj) {
+        return obj.id != sportsmanId;
+      });
+      crud.setState({...crud.state, items});
+    },
+
+    /* Редактирование строк*/
+
+    replaceInp: function replaceInp(editingId) {
+      crud.setState({...crud.state, editingId});
+    }, /* Сохранение строк*/
+    saveItems: function saveSportsman(id) {
+      const newSportsman = {
+          name: $('.inpReplace1').val(),
+          sport: $('.inpReplace2').val(),
+          salary: $('.inpReplace3').val(),
+          id,
+      };
+      const items = crud.state.items.map(s => s.id == id ? newSportsman : s);
+      crud.setState({...crud.state, items, editingId: 0})
+    },
+
+    // Отрисовываем таблицу
+
+    renderRow: (items) => {
+      const isEditingRow = items.id == crud.state.editingId;
+      return `<div>
+      <div class="cell">${isEditingRow ? `<input class="inpReplace1" value='${items.name}'>` : items.name}</div>
+      <div class="cell">${isEditingRow ? `<input class="inpReplace2" value='${items.sport}'>` : items.sport}</div>
+      <div class="cell">${isEditingRow ? `<input class="inpReplace3" value='${items.salary}'>` : items.salary}</div>
+      <div class="cell">${items.id}</div>
+      <button class="delSportsman" data-id='${items.id}'>Удалить спортсмена</button>
+      ${ isEditingRow ?
+         `<button class="save" data-id='${items.id}'> Сохранить</button>`
+          : `<button class="upDate" data-id='${items.id}'> Редактировать</button>`}
+      </div>`;
+    },
+
+    renderTable: () => `
+      ${crud.renderHeaders()}
+      ${crud.state.items.map(crud.renderRow).join('')}
+    `,
+
+    renderHeaders: () => {
+      return `<div class="table-header">
+      <div class="cell">Имя <button class="sortBut"> ${crud.state.sort.direction === 'desc' ? "Я-А" :"А-Я"}</button"></div>
+      <div class="cell">Спорт</div>
+      <div class="cell">Заработок</div>
+      <div class="cell">Номер</div>
+      </div>`;
+    },
+
+    render: () => {
+      $('.table').html(crud.renderTable);
+    },
+  }; // <-------crud
+
+  // Работа с кнопками
+
+  $('.but').bind('click', crud.addSportsman);
+  $('body').on('click','.sortBut', crud.choiseMehtodSort);
+  $('body').on('click','.delSportsman', () => crud.deleteSportsman(event.target.dataset.id));
+  $('body').on('click','.upDate', () => crud.replaceInp(event.target.dataset.id));
+  $('body').on('click','.save', () => crud.saveItems(event.target.dataset.id));
+
+  return crud;
+}; //<---------Crud();
+
+const items = [
+  {
+    name:   'Лионель Месси',
+    sport:  'Футбол',
+    salary: '$ 127 млн',
+    id: '1',
+  }, {
+    name:   'Криштиану Роналду',
+    sport:   'Футбол',
+    salary:  '$ 109 млн',
+    id: '2',
+  }, {
+    name:   'Неймар',
+    sport:  'Футбол',
+    salary: '$ 105 млн',
+    id: '3',
+  }, {
+    name:   'Канело Альварес',
+    sport:  'Бокс',
+    salary: '$ 94 млн',
+    id: '4',
+  }, {
+    name:   'Роджер Федерер',
+    sport:  'Большой тенис',
+    salary: '$ 93,4 млн',
+    id: '5',
+  }, {
+    name:   'Рассел Уилсон',
+    sport:  'Американский футбол',
+    salary: '$ 89,5 млн',
+    id: '6',
+  }, {
+    name:   'Аарон Роджерс',
+    sport:  'Американский футбол',
+    salary: '$ 89,3 млн',
+    id: '7',
+  }, {
+    name:   'Леброн Джеймс',
+    sport:  'Баскетбол',
+    salary: '$ 89 млн',
+    id: '8',
+  }, {
+    name:   'Стивен Карри',
+    sport:  'Баскетбол',
+    salary: '$ 79,8 млн',
+    id: '9',
+  }, {
+    name:   'Кевин Дюран',
+    sport:  'Баскетбол',
+    salary: '$ 65,4 млн',
+    id: '10',
   },
-  sortStringRevers: function sortStringRevers() {
-    const sportsman = [...state.sportsman.sort((a, b) => a.name > b.name ? 1 : -1)];
-    const sort = {...state.sort, direction: 'desc' };
-    setState({...state, sportsman, sort});
-  },
-  choiseMehtodSort: function choiseMehtodSort() {
-      if (state.sort.direction === 'desc') {
-        state.sortString();
-      } else {
-        state.sortStringRevers();
-      }
-  }, /* Удаление строк*/
-  deleteSportsman: function deleteSportsman(sportsmanId) {
-    const sportsman = state.sportsman.filter(function(obj) {
-      return obj.id != sportsmanId;
-    });
-    setState({...state, sportsman});
-  }, /* Редактирование строк*/
-  replaceInp: function replaceInp(editingId) {
-    setState({...state, editingId});
-  }, /* Сохранение строк*/
-  saveSportsman: function saveSportsman(id) {
-    const newSportsman = {
-        name: $('.inpReplace').val(),
-        sport: $('.inpReplace').val(),
-        salary: $('.inpReplace').val(),
-        id: $('.inpReplace').val(),
-    }
-  },
-
-  editingId: 0,
-}
-
-  // Отрисовываем таблицу
-
-let lastId = 10;
-
-const setState = (newState) => {
-  if (state != newState) {
-    state = newState;
-    render();
-  }
-}
-
-const renderRow = (sportsman) => {
-  const isEditingRow = sportsman.id == state.editingId;
-  return `<div>
-  <div class="cell">${isEditingRow ? `<input class="inpReplace" value='${sportsman.name}'>` : sportsman.name}</div>
-  <div class="cell">${isEditingRow ? `<input class="inpReplace" value='${sportsman.sport}'>` : sportsman.sport}</div>
-  <div class="cell">${isEditingRow ? `<input class="inpReplace" value='${sportsman.salary}'>` : sportsman.salary}</div>
-  <div class="cell">${sportsman.id}</div>
-  <button class="delSportsman" data-id='${sportsman.id}'>Удалить спортсмена</button>
-  ${ isEditingRow ?
-     `<button class="save" data-id='${sportsman.id}'> Сохранить</button>`
-      : `<button class="upDate" data-id='${sportsman.id}'> Редактировать</button>`}
-  </div>`;
-};
-
-const renderTable = () => `
-  ${renderHeaders()}
-  ${state.sportsman.map(renderRow).join('')}
-`
-
-const renderHeaders = () => {
-  return `<div class="table-header">
-  <div class="cell">Имя <button class="sortBut"> ${state.sort.direction === 'desc' ? "Я-А" :"А-Я"}</button"></div>
-  <div class="cell">Спорт</div>
-  <div class="cell">Заработок</div>
-  <div class="cell">Номер</div>
-  </div>`;
-}
-
-const render = () => {
-  $('.table').html(renderTable());
-
-}
+];
 
 window.onload = function() {
-  render();
+  const table = document.getElementsByClassName('table')[0];
+  const crud = CRUD(items,table);
+  crud.render();
 };
-
-$('.but').bind('click', state.addSportsman);
-
-// Работа с кнопками
-
-$('body').on('click','.sortBut', state.choiseMehtodSort);
-$('body').on('click','.delSportsman', () => state.deleteSportsman(event.target.dataset.id));
-$('body').on('click','.upDate', () => state.replaceInp(event.target.dataset.id));
-$('body').on('click','.save', () => state.saveSportsman(event.target.dataset.id));
-
-  const sportsman = state.sportsman.map(s => s.id == id ? newSportsman : s);
-  setState({...state, sportsman, editingId: 0})
